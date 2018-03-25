b3b0ac6b95e843e1dccf52cb848002e38853c4ba
ghost <ghost@konpa.ku>
2018-03-25 05:09:21 +0000

    0.1.0 - eab0e1b7a148c438add4cedf79da0f16981fb8f9

---
+++ implementors/core/iter/iterator/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["i2c_linux"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/iterator/trait.Iterator.html\" title=\"trait core::iter::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"i2c_linux/struct.Enumerator.html\" title=\"struct i2c_linux::Enumerator\">Enumerator</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
